e1ea4349a485542fd377eb4fabf2673d82998618
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-19 19:42:27 +0200

    Update database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
 ActiveRecord::Base.transaction do
-  syme = FactoryGirl.create(:user, name: 'Syme', email: 'syme@evaporated.oc')
-
-  salesmen = FactoryGirl.create_list(:salesman, 10)
+  salesmen  = FactoryGirl.create_list(:salesman, 10)
   customers = FactoryGirl.create_list(:customer, 100)
 
-  FactoryGirl.create_list(:orders, 100)
-
-
+  FactoryGirl.create_list(:orders, 500,
+                          salesman: salesmen[rand(0..salesmen.count - 1)],
+                          customers: customers[rand(0..customers.count - 1)])
 end

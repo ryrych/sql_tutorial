1a2db74bad072db17fd2ab23a771cf8a8c604dac
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-19 19:31:38 +0200

    Add factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,10 +1,21 @@
 FactoryGirl.define do
+  factory :salesman do
+    name { FFaker::Name.name }
+    city { FFaker::Address.city }
+    commission { rand(1..100) }
+  end
+
+  factory :customer do
+    cust_name { FFaker::Name.name }
+    city { FFaker::Address.city }
+    grade { rand(100..500) }
+  end
+
   factory :order do
-    ord_no
     purch_amt { rand(500..2000) }
     ord_date { FFaker::Time.date }
 
-    # customer
-    # salesman
+    customer
+    salesman
   end
 end

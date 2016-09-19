b552729c13cf4a821789f38080d18458a3a1115c
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2016-09-19 19:44:15 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ ActiveRecord::Base.transaction do
   salesmen  = FactoryGirl.create_list(:salesman, 10)
   customers = FactoryGirl.create_list(:customer, 100)
 
-  FactoryGirl.create_list(:orders, 500,
+  FactoryGirl.create_list(:order, 500,
                           salesman: salesmen[rand(0..salesmen.count - 1)],
-                          customers: customers[rand(0..customers.count - 1)])
+                          customer: customers[rand(0..customers.count - 1)])
 end
